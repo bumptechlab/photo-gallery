c2205a55eb28a03c849635b6b6509949a02407ef
Barney <barney@gmail.com>
2024-12-10 16:58:07 +0800

    chore: 安装live-updates功能

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,17 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
-  webDir: 'www'
+  appId: 'com.ionic.photo.gallery',
+  appName: 'Photo-Gallery',
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      appId: '7a19e700',
+      channel: 'a-vn2-sprvn201-website',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
+  }
 };
 
 export default config;
